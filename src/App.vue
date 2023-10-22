cf2ea03106e99fc0cfea775a2032ef9cdc91560a
Cesar Lai <git@cesarlai.com>
2023-10-22 12:15:02 +0800

    feat: add CSS Modules example

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup>
+import styles from './app.module.less'
+</script>
 
 <template>
-  <div>
+  <div :class="styles.app">
     <h1>Hello world</h1>
   </div>
 </template>
